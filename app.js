f3bc547afe47d04d0684e9be34e7cc0370d9b045
mohith555 <mohithkrishna55555@gmail.com>
2024-07-12 09:33:21 +0530

    adding files

--- app.js
+++ app.js
@@ -12,4 +12,46 @@
 // }
 // console.log(system)
 
-console.log("rey run aindhi ra ewww ahaaa")
+//reading and writing files in asynchoronous
+// const {readFile,writeFile, read}=require('fs');
+// const util=require('util');
+// let readfilepromise=util.promisify(readFile);
+// let writefilepromise=util.promisify(writeFile);
+// async function Method(){
+//     try{
+//         console.log(1);
+//         const first=await readfilepromise('./fsmodule/syncmodule/first.txt','utf-8');
+//         const second=await readfilepromise('./fsmodule/syncmodule/second.txt','utf-8');
+//         console.log(first,second);
+//         await writefilepromise('./displayasynpromise',`${first} and ${second}`);
+//         console.log(2);
+//     }
+//     catch(err){
+//         console.log(err);
+//     }
+// }
+// Method();
+
+//or same we can skip some step like this below that modify upper one
+// const {readFile,writeFile, read}=require('fs').promises;
+// async function Method(){
+//     try{
+//         console.log(1);
+//         const first=await readFile('./fsmodule/syncmodule/first.txt','utf-8');
+//         const second=await readFile('./fsmodule/syncmodule/second.txt','utf-8');
+//         console.log(first,second);
+//         await writeFile('./displayasynpromise',`${first} and ${second}`);
+//         console.log(2);
+//     }
+//     catch(err){
+//         console.log(err);
+//     }
+// }
+// Method();
+
+const eventEmitter=require('events');
+const customemitter=new eventEmitter();
+customemitter.on('meth',()=>{
+    console.log("hi this is emitter");
+})
+customemitter.emit('meth');
